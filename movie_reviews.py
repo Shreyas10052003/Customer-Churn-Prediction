57bdced9811806c06d62337d19046301e9b6e004
Datamites <shreyassathyanarayanan@gmail.com>
2025-05-30 12:01:14 +0530

    Added only House Price Prediction notebook

--- movie_reviews.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open('stored_data.pkl', 'rb') as pickle_file:
-    reviews = pickle.load(pickle_file)
-    print("Data loaded successfully:", reviews)  # Optional: Print to verify
-
